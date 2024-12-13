aefeaf4c2c392f773cf7b5b7bbfb9060c5c186c4
Josim <22josimuddin06@gmail.com>
2024-12-14 01:15:46 +0600

    Updated new data and responsive

---
+++ script.js
@@ -0,0 +1,9 @@
+let navbar = document.getElementById("navbar")
+navbar.style.maxHeight = "0";
+
+function togglemenu(){
+  if(navbar.style.maxHeight = "0")
+  {
+    navbar.style.maxHeight = "300px"
+  }
+  }
